86825f57b4bf5f12cd8cfe830cba2fe467336ac4
Ryan Roat <ryanroat@gmail.com>
2021-07-14 10:34:36 -0400

    intro useEffect demo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Toggle } from "./Toggle";
 
 const App = () => {
-
   const [name, setName] = useState('')
+
+  useEffect(() => {
+    document.title = name
+  })
+
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
       <Toggle />
       <form onSubmit={(e) => {
         e.preventDefault();
-        fromSubmit(name, setName)
       }}>
         <input type="text" onChange={(e) => {setName(e.target.value)}} value={name} />
         <button>Submit</button>
@@ -18,10 +21,5 @@ const App = () => {
     </div>
   );
 };
-
-const fromSubmit = (value, setValue) => {
-  console.log(`email sent to ${value}`);
-  setValue('');
-}
 
 export default App;
