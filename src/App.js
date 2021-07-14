189851be5a7ce91187e74c96b9f59cb302864030
Ryan Roat <ryanroat@gmail.com>
2021-07-13 20:18:17 -0400

    controlled input demo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+
+  const [name, setName] = useState('')
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <input type="text" onChange={(e) => {setName(e.target.value)}} value={name} />
     </div>
   );
 };
