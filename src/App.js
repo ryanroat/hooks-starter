f1a3c690d92d34972c661a07d3ce23eaf1b4102b
Ryan Roat <ryanroat@gmail.com>
2021-07-13 20:33:57 -0400

    toggle button component with state demo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Toggle } from "./Toggle";
 
 const App = () => {
 
@@ -6,6 +7,7 @@ const App = () => {
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
+      <Toggle />
       <input type="text" onChange={(e) => {setName(e.target.value)}} value={name} />
     </div>
   );
